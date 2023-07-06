5dd823220c870cb691a4287f4d16dfda2d90435c
Fernando <death_dan@hotmail.com>
2023-07-06 10:11:32 -0300

    Ajustando ambiente App

--- source/App/App.php
+++ source/App/App.php
@@ -4,18 +4,27 @@ namespace Source\App;
 
 use Source\Core\Controller;
 use Source\Models\Auth;
+use Source\Models\Report\Access;
+use Source\Models\Report\Online;
+use Source\Models\User;    
 use Source\Support\Message;
 
 class App extends Controller
 {
+    /** @var User */
+    private $user;
+
     public function __construct()
     {
-        parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_APP);
+        parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_APP . "/");
 
-        if (!Auth::user()) {
+        if (!$this->user = Auth::user()) {
             $this->message->warning("Efetue o login para acessar o APP")->flash();
             redirect("/entrar");
         }
+
+        (new Access())->report();
+        (new Online())->report();
     }
 
     public function home(): void
--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -334,23 +334,24 @@ function redirect(string $url): void
 /**
  * theme
  *
- * @param  mixed $path
+ * @param  string|null $path
+ * @param string $theme
  * @return string
  */
-function theme(string $path = null): string
+function theme(string $path = null, string $theme = CONF_VIEW_THEME): string
 {
     if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
         if ($path) {
-            return CONF_URL_TEST . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+            return CONF_URL_TEST . "/themes/{$theme}/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
         }
-        return CONF_URL_TEST . "/themes/" . CONF_VIEW_THEME;
+        return CONF_URL_TEST . "/themes/{$theme}";
     }
 
     if ($path) {
-        return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        return CONF_URL_BASE . "/themes/{$theme}/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
     }
 
-    return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME;return CONF_URL_BASE;
+    return CONF_URL_BASE . "/themes/{$theme}";
 }
 
 /**
